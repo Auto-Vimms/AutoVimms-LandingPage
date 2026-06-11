25fe6642dace739ceedb5c550b647559b85879de
massatoMMKP <massato.kanenoo@gmail.com>
2026-06-10 23:13:04 -0300

    feat: add favicon

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  whatsapp: '5511945861978', // replace with official number (DDI + DDD + number)
+  whatsapp: '5511989397563', // replace with official number (DDI + DDD + number)
   email: 'autovimms@gmail.com',
   instagram: 'autovimms',
   submitMethod: 'whatsapp', // 'whatsapp' | 'email'
